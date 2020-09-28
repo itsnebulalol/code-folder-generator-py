c7ed6a8864639174f55d67431d267b657fd6475a
Dominic <18669106+itsnebulalol@users.noreply.github.com>
2020-09-27 21:30:00 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+print("|=============================|")
+print("| Generating a code folder... |")
+print("|=============================|")
+print(" ")
+
+# name
+print(" - Please choose a project name:")
+name = input()
+print(" ")
+
+print(" - Generating, please wait...")
+os.mkdir(os.getcwd() + os.path.sep + name)
+os.system("git init ." + os.path.sep + name)
+print(" ")
+
+print(" - Opening in Visual Studio Code...")
+os.system("code ." + os.path.sep + name)
+print(" ")
+print(" ")
+
+print("|===================|")
+print("| Finished process! |")
+print("|===================|")
